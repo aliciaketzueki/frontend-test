bf8e3c1406119b38721d74b7f2950f749101e742
ketzueki <aliciaketzueki@gmail.com>
2019-10-23 05:05:10 +0300

    Исправляет баг слайдера с переходом на моб.версию

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -5,32 +5,69 @@
     return;
   }
 
-  let swiper = new Swiper('.slider .swiper-container', {
-    slidesPerView: 3,
-    slidesPerColumn: 2,
-    spaceBetween: 40,
-    pagination: {
-      el: '.slider .swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.slider .swiper-button-next',
-      prevEl: '.slider .swiper-button-prev',
-    },
-    breakpoints: {
-      1279: {
-        slidesPerView: 2,
-        //spaceBetween: 30
-      },
+  let swiperDesktop = undefined;
+  let swiperMobile = undefined;
 
-      375: {
-        //slidesPerView: 2,
-        //spaceBetween: 20
-      },
-      // when window width is >= 480px
-      
-      // when window width is >= 640px
+  // Слайдер на десктопе
+  function initSlider () {
+    if (window.matchMedia(`(min-width: 768px)`).matches) {
+      if (swiperDesktop == undefined) {
+        swiperDesktop = new Swiper('.slider .swiper-container', {
+          slidesPerView: 3,
+          slidesPerColumn: 2,
+          spaceBetween: 40,
+          observer: true,
+          observeParents: true,
+          observeSlideChildren: true,
+          pagination: {
+            el: '.slider .swiper-pagination',
+            clickable: true,
+          },
+          navigation: {
+            nextEl: '.slider .swiper-button-next',
+            prevEl: '.slider .swiper-button-prev',
+          },
+          breakpoints: {
+            1279: {
+              slidesPerView: 2,
+            },
+          }
+        });
+      }
+
+      if (swiperMobile != undefined) {
+        swiperMobile.destroy();
+        swiperMobile = undefined;
+      }
+    } else if (window.matchMedia(`(max-width: 767px)`).matches) {
+      if (swiperDesktop != undefined) {
+        swiperDesktop.destroy();
+        swiperDesktop = undefined;
+      }
+
+      if (swiperMobile == undefined) {
+        swiperMobile = new Swiper('.slider .swiper-container', {
+          slidesPerView: 1,
+          slidesPerColumn: 1,
+          spaceBetween: 16,
+          centeredSlides: true,
+          loop: true,
+          observer: true,
+          observeParents: true,
+          observeSlideChildren: true,
+          pagination: {
+            el: '.slider .swiper-pagination',
+            clickable: true,
+          },
+        });
+      }
     }
+  }
+
+  initSlider();
+
+  window.addEventListener(`resize`, function () {
+    initSlider();
   });
 
 })();
